eab03ed89c44170045f904b72f0680f7e125faa3
krishnadontaraju <69509421+krishnadontaraju@users.noreply.github.com>
2022-11-09 17:47:44 -0700

    Added team selection

---
+++ js/selection.js
@@ -0,0 +1,56 @@
+let teamNames = ["CSK", "DC", "GT", "KKR", "LSG", "IPL", "MI", "", "PK", "RCB", "RR", "SRH"]
+let teamColor = ["#FFFF00", "#191970", "#87CEEB", "#4B0082", "#00FFFF", "#FFD700", "#0000FF", "#FFFFFF", "#FF0000", "#8B0000", "#FF1493", "#FF8C00"]
+
+var margin = {
+    top: 30,
+    right: 20,
+    bottom: 20,
+    left: 50
+}
+
+let width = 850
+let height = 350
+
+let hexColumns = 4
+let hexRows = 3
+
+let hexRadius = d3.min([width/((hexColumns + 0.5) * Math.sqrt(3)), height/((hexRows + 1/3) * 1.5)]);
+
+width = hexColumns * hexRadius * Math.sqrt(3);
+height = hexRows * 1.5 * hexRadius + 0.5 * hexRadius;
+
+let hexbin = d3.hexbin().radius(hexRadius);
+
+let points = []
+for (let i = 0; i < hexRows; i++) {
+    for (let j = 0; j < hexColumns; j++) {
+        let x = hexRadius * j * Math.sqrt(3)
+        if(i%2 === 1){
+            x += (hexRadius * Math.sqrt(3)) / 2
+        }
+        let y = hexRadius * i * 1.5
+        points.push([x,y])
+    }
+}
+
+let svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+svg.append("g")
+    .selectAll(".hexagon")
+    .data(hexbin(points))
+    .enter()
+    .append("path")
+    .attr("class", "hexagon")
+    .attr("d", function (d) {
+        return "M" + d.x + "," + d.y + hexbin.hexagon();
+    })
+    .attr("stroke", "white")
+    .attr("stroke-width", "1px")
+    .attr("fill", function (d,i) {
+		return teamColor[i];
+	});
